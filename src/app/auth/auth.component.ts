1a104bfbbe878bc2d9e2a40eab8bdfbc5d007631
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2019-11-16 12:27:18 -0300

    Divisão de app module em varios modulos e lazy loading

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -52,4 +52,8 @@ export class AuthComponent implements OnInit {
 
     form.reset();
   }
+
+  onHandlerError() {
+    this.error = null;
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 import { Subscription } from 'rxjs';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -13,11 +14,13 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
   private idChangeSub: Subscription;
 
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService,
+              private loggingService: LoggingService) { }
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
     this.updateList();
+    this.loggingService.printLog('Hello from Shopping List');
   }
 
   ngOnDestroy() {
